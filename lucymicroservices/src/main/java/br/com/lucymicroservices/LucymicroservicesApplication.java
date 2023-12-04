ab88a990abdaaa83ed56c20bfd6cce922f497ff6
Lucian Bellini <lucianbellini1@gmail.com>
2023-12-04 10:47:00 -0300

    04-12

---
+++ lucymicroservices/src/main/java/br/com/lucymicroservices/LucymicroservicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucymicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucymicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucymicroservicesApplication.class, args);
+	}
+
+}
---
+++ lucymicroservices/src/test/java/br/com/lucymicroservices/LucymicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucymicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucymicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
